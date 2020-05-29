0fbc349f9b2b3b22a3b7c63e48aff8f20c5d29a2
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-05-29 16:02:51 +0530

    Create Shape.java

---
+++ ShapeCalculator/Shape.java
@@ -0,0 +1,10 @@
+/* this file will store 
+   all the abstract methods */
+
+abstract class Shape{
+  public abstract void circle();  //abstract methods donot have bodies
+  public abstract void rectangle();
+  public abstract void square();
+  public abstract void triangle();
+
+}
