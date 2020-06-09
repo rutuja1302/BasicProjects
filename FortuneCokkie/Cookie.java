45c16ab4f49bb53bf76ea64a86370d08a4446635
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-06-09 20:55:33 +0530

    Create Cookie.java

---
+++ FortuneCokkie/Cookie.java
@@ -0,0 +1,16 @@
+//Fortune cookie teller
+
+import java.util.Random;
+
+public class Main{
+  public static void main(String [] args){
+    System.out.println("<===Fortune Cookies===> \n");
+    System.out.println("Your fortune cookie says: \n ");
+    String[] fortune = {"Enjoy each day","Cherish your family","Always stay positive","Bring sunshine to others","Enjoy little things!","Strive for sucess","Always work hard","Good things are coming","Honesty is the greatest asset","Do what you love","Never Quit!","Do big!","Live your dream","Faith will get you everywhere","A journey of a thousand miles begins with a single step!","You are the best","Happiness is an activity","If you feel you are right, stand firmly by your convitions","It's now or never!"};
+    
+    Random obj = new Random();
+    int myNum = obj.nextInt(fortune.length)+1;
+    
+    System.out.println(fortune[myNum]);
+  }
+}
