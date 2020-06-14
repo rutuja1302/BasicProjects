32aaac92aeaf5575cdc2b8015d8188596462e587
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-06-14 16:32:51 +0530

    Create Count.java

---
+++ CountVowels/Count.java
@@ -0,0 +1,19 @@
+/*A simple program to count vowels */
+
+import java.util.Scanner;
+public class Main{
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String str = scan.nextLine().toLowerCase();
+    char[] arr = str.toCharArray();
+    char[] vowels = {'a','e','i','o','u'};
+    int count=0;
+    for(int i=0 ; i< arr.length ; i++){
+      if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o' || arr[i]=='u'){
+        count++;
+      }
+    }
+    System.out.println("Number of vowels: " +count);
+  }
+}
