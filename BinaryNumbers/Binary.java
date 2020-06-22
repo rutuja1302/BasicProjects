cddb7b0533c16b9ff850b17e1cfe27efd8ff9721
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-06-22 21:18:55 +0530

    Update Binary.java

--- BinaryNumbers/Binary.java
+++ BinaryNumbers/Binary.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+import java.lang.Math;
+public class Main{
+  private static final Scanner scanner = new Scanner(System.in);
+  public static void main(String [] args){
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int n = scanner.nextInt();
+    String binary = Integer.toBinaryString(n);
+    String[] arr = binary.split("0");
+    for(String a: arr){
+      int size = a.length();
+      list.add(size);
+    }
+    int max = Collections.max(list);
+    System.out.println(max);
+  }
+}
