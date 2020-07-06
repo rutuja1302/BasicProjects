6a7310dd251e7917a580d5cd6924d3b197a1aeca
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-07-06 21:31:11 +0530

    Update Main.java

--- LibraryFine/Main.java
+++ LibraryFine/Main.java
@@ -1,1 +1,29 @@
-
+import java.util.Scanner;
+public class Main{
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    int fine = 0;  //initializing fine
+    //scanning actual return date, month, year
+    int da = scan.nextInt();
+    int ma = scan.nextInt();
+    int ya = scan.nextInt();
+    //scanning expected return date, month, year
+    int de = scan.nextInt();
+    int me = scan.nextInt();
+    int ye = scan.nextInt();
+    if(ya == ye){
+      if(ma == me){
+        if(da == de){
+          fine = 0;
+        }else if(da > de){
+          fine = 15 * (da-de);
+        }
+      }else if(ma>me){
+        fine = 500 * (ma-me);
+      }
+    }else if(ya > ye){
+      fine = 1000;
+    }
+    System.out.println(fine);
+  }//end of main 
+}//end of class
