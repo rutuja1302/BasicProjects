d0f4a7a4bb6b61e2afd57b01dc7bc955fc861f58
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-07-11 20:26:25 +0530

    Update Main.java

--- PatternSyntaxChecker/Main.java
+++ PatternSyntaxChecker/Main.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+public class Main{
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+    //scanning number of test cases
+    int testCases = scan.nextInt();
+    scan.nextLine();
+    while(testCases > 0){
+      String pattern = scan.nextLine();
+      try{
+        Pattern.compile(pattern);
+        System.out.println("Pattern Valid");
+      }catch(PatternSyntaxException e){
+        System.out.println("Pattern Invalid");
+      }
+      testCases--;
+    }
+  }
+}
