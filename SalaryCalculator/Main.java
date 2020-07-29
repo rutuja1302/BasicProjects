c72574d96dbbc3881229d35fb213da1e03d3da46
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-07-29 19:14:56 +0530

    Update Main.java

--- SalaryCalculator/Main.java
+++ SalaryCalculator/Main.java
@@ -1,1 +1,26 @@
-
+//Employee's Salary Calculator
+import java.util.Scanner;
+public class Main{
+  public static int salaryCalculator(int hoursPerWeek, int amountPerHour, int vacDays){
+    if(hoursPerWeek < 0){
+      return -1;
+    }
+    if(amountPerHour < 0){
+      return -1;
+    }
+    int weeklyPayCheck = hoursPerWeek * amountPerHour;
+    int unpaidCheck = vacDays*amountPerHour*8;
+    return (weeklyPayCheck*52)-unpaidCheck;
+  }
+  public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number of hours the employee works per week: ");
+    int hours = input.nextInt();
+    System.out.print("Amount of money the employee makes per hour: ");
+    int amt = input.nextInt();
+    System.out.print("No. of vaccations: ");
+    int vac = input.nextInt();
+    int salary = salaryCalculator(hours,amt,vac);
+    System.out.println("Total Salary: " +salary);
+  }
+}
