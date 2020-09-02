7b6b9d4ec52ceffdf630c61369adf3be7975f5d2
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-02 19:25:02 +0530

    Update Solution.java

--- Comparator/Solution.java
+++ Comparator/Solution.java
@@ -1,1 +1,21 @@
+import java.util.*;
+public class Solution {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
