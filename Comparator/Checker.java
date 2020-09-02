5eda872e2965a9035ffa0eeaa95dfdf528b1bc30
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2020-09-02 19:23:33 +0530

    Update Checker.java

--- Comparator/Checker.java
+++ Comparator/Checker.java
@@ -1,1 +1,13 @@
+import java.util.*;
 
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+        int res = b.score - a.score;
+        if(res==0){
+            return a.name.compareTo(b.name);
+        }else{
+            return res;
+        }
+    }
+}
